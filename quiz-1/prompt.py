5e3f8f1a700fc6656918ac99f755e61b91e3463c
kumaramardeep342 <kumaramardeep342@gmail.com>
2026-06-12 19:02:15 +0000

    added quiz prompt

---
+++ quiz-1/prompt.py
@@ -0,0 +1,18 @@
+# Generate 25 question for below given instruction and attached document
+# question type :
+# MCQ - Multiple choice question , only one answer is correct
+# MSQ - Multiple Select question , more than one answer is correct
+
+# question format :
+# based on direct concept , functions which is available and what it's work
+# based on scenario's like I am data engineer , ml engineer etc. I need to build data pipeline includes processing and orchestration which I should use
+# option : Apache Spark and Airflow etc.
+# based on code snippet and select the output
+# numerical calculation based on concept
+
+# practice session question prof given and in exam you can expect difficulty level bit harder than this.
+# Which Spark component is responsible for scheduling tasks and converting operations into DAG computations?
+# A.Spark Executor
+# B.Cluster Manager
+# C.Spark Driver
+# D.Saprk SQL
